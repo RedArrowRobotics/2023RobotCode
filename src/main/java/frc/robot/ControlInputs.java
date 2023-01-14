2fad45c0bbf452af73a1330a0367a26c9bf0d64d
Superlamaface <39967561+Superlamaface@users.noreply.github.com>
2023-01-14 11:22:29 -0500

    Addition of drive code.

---
+++ src/main/java/frc/robot/ControlInputs.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class ControlInputs {
+    
+    //Joysticks IDs
+    private final int driveStickDeviceId = 0;
+
+    //Joystick Definitions
+    private final Joystick driveStick = new Joystick(driveStickDeviceId);
+    
+    //Variable Defintions
+    public double driveStickX = 0.0;
+    public double driveStickY = 0.0;
+
+    //Reading the controls
+    public final void readControls() {
+        driveStickX = driveStick.getX();
+        driveStickY = driveStick.getY();
+    }
+}
