f3a3060d2828b5da7ff7d968ae786d4f02761bc2
Superlamaface <39967561+Superlamaface@users.noreply.github.com>
2023-01-21 15:47:49 -0500

    Changed time to function of distance

--- src/main/java/frc/robot/Motion.java
+++ src/main/java/frc/robot/Motion.java
@@ -3,6 +3,7 @@ package frc.robot;
 public class Motion {
 
     // Scaler value that will be applied to the power output (significantly less then one)
+    //This Scaler takes velocity and translates it into pwm
     private double moveConstant;
 
     // Total time of the move in seconds
@@ -15,18 +16,21 @@ public class Motion {
     private double tolerance;
 
     public Motion(double time, double dist, double tol, double moveK){
-        timeMax = time;
+        timeMax = Time(dist);
         distance = dist;
         tolerance = tol;
         moveConstant = moveK;
-        //moveConstant = 2/dist;
+    }
+
+    private double Time(double maxDistance) {
+        return (2*maxDistance)/(5500/60);
     }
     
     /*
         Returns a double with an output range of [-1.0, 1.0] as the motor power by changing the target distance to be along
         a polynomial curve according to the current time of the move
     */
-    public double getPower(double currentPosition, double currentTime){
+    public double getPower(double currentPosition, double currentTime) {
         double targetPosition = distance * polynomial345(currentTime / timeMax);
         return getFeedbackPower(currentPosition, targetPosition);
     }
@@ -36,27 +40,27 @@ public class Motion {
     }
 
     // Returns a power value between -1.0 and 1.0 for the power of the motor
-    private double getFeedbackPower(double currentPosition, double targetPosition){
+    private double getFeedbackPower(double currentPosition, double targetPosition) {
         double power = moveConstant * Math.sqrt(Math.abs(targetPosition - currentPosition));
-        if (targetPosition - currentPosition < 0.0){
+        if (targetPosition - currentPosition < 0.0) {
             power *= -1.0;
         }
-        if (power < -1.0){
+        if (power < -1.0) {
             return -1.0;
-        } else if (power > 1.0){
+        } else if (power > 1.0) {
             return 1.0;
         } else {
             return power;
         }
     }
-    public double getFeedbackPower(double currentPosition, double targetPosition, double moveConstant){
+    public double getFeedbackPower(double currentPosition, double targetPosition, double moveConstant) {
         double power = moveConstant * Math.sqrt(Math.abs(targetPosition - currentPosition));
-        if (targetPosition - currentPosition < 0.0){
+        if (targetPosition - currentPosition < 0.0) {
             power *= -1.0;
         }
-        if (power < -1.0){
+        if (power < -1.0) {
             return -1.0;
-        } else if (power > 1.0){
+        } else if (power > 1.0) {
             return 1.0;
         } else {
             return power;
@@ -67,7 +71,7 @@ public class Motion {
         Boolean value that is true if either the current time of the move is greater than the max time of the move,
         or if the distance away from the target position is within the tolerance of error for the position
     */
-    public boolean isDone (double currentPosition, double currentTime){
+    public boolean isDone (double currentPosition, double currentTime) {
         return (currentTime > timeMax) || (Math.abs(currentPosition - distance) < tolerance);
     }
 }
