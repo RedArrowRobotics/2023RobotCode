cae4b9c758000eaacc4f2c13292176a8ecce9c3f
Douglas Hudson <dougjoe2e@gmail.com>
2023-02-04 14:11:01 -0500

    added safety limit for drive to charge station

--- src/main/java/frc/robot/Auto/AutoActionDriveToChargingStation.java
+++ src/main/java/frc/robot/Auto/AutoActionDriveToChargingStation.java
@@ -9,16 +9,27 @@ public class AutoActionDriveToChargingStation extends AutoAction {
     private final double maxDrivePower = .2;
     private final double powerIncrementPerCycle = .012;
     private final float pitchLimit = -10;
+    
+    private double initialLeftEncoderCount = 0f;
+    private double initialRightEncoderCount = 0f;
+    private final double encoderDeltaLimit = 150;
     @Override
     public void Init(DriveTrain driveTrain, Components components, SensorInputs sensor) {
-        
+        initialLeftEncoderCount = driveTrain.getFrontLeftPosition();
+        initialRightEncoderCount = driveTrain.getFrontRightPosition();
     }
 
     @Override
     public boolean Execute(DriveTrain driveTrain, Components components, SensorInputs sensor) {
         driveTrain.arcadeDrive(drivePower, 0);
         drivePower = Math.max(maxDrivePower, drivePower + powerIncrementPerCycle);
-        return sensor.currentPitchDegrees < pitchLimit;
+        
+        boolean beyondPitchLimit =  sensor.currentPitchDegrees < pitchLimit;
+        boolean pastEncoderDeltaLimit = 
+            ( driveTrain.getFrontLeftPosition() > (initialLeftEncoderCount + encoderDeltaLimit) ) ||
+            ( driveTrain.getFrontRightPosition() > (initialRightEncoderCount + encoderDeltaLimit) );
+
+        return beyondPitchLimit || pastEncoderDeltaLimit;
     }
 
     @Override
