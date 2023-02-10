e42eb8e35257c63cabe33a3aaf8ffacd4e054b9a
Superlamaface <39967561+Superlamaface@users.noreply.github.com>
2023-02-09 20:54:34 -0500

    Updated Libraries and changed belt code to use variable

--- src/main/java/frc/robot/ComponentsControl.java
+++ src/main/java/frc/robot/ComponentsControl.java
@@ -3,6 +3,7 @@ package frc.robot;
 public class ComponentsControl {
 
     private final double beltK = 0.5;
+    private final double beltGyroRotaionInsideDegreeToleranceFromY = 70; //From ±90 degrees in each direction from start point
     
     public void runComponents(Components components, ControlInputs controlInputs, SensorInputs sensorInputs) {
         
@@ -13,9 +14,9 @@ public class ComponentsControl {
         //Controls
             //Belts
         if (controlInputs.beltAuto) {
-            if (sensorInputs.currentYawDegrees <= -20.0 && sensorInputs.currentYawDegrees >= -160.0){
+            if (sensorInputs.currentYawDegrees <= -(90-beltGyroRotaionInsideDegreeToleranceFromY) && sensorInputs.currentYawDegrees >= -(90+beltGyroRotaionInsideDegreeToleranceFromY)) {
                 mainSideBeltSpeed = beltK;
-            } else if (sensorInputs.currentYawDegrees >= 20.0 && sensorInputs.currentYawDegrees <= 160.0){
+            } else if (sensorInputs.currentYawDegrees >= (90-beltGyroRotaionInsideDegreeToleranceFromY) && sensorInputs.currentYawDegrees <= (90+beltGyroRotaionInsideDegreeToleranceFromY)) {
                 mainSideBeltSpeed = -beltK;
             }
         } else {
