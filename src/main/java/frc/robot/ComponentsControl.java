edb4d136c02605bbdb289fda4adb982533a7d6a0
Superlamaface <39967561+Superlamaface@users.noreply.github.com>
2023-02-11 11:59:47 -0500

    Fixed spelling mistake and added spaces to math

--- src/main/java/frc/robot/ComponentsControl.java
+++ src/main/java/frc/robot/ComponentsControl.java
@@ -3,7 +3,7 @@ package frc.robot;
 public class ComponentsControl {
 
     private final double beltK = 0.5;
-    private final double beltGyroRotaionInsideDegreeToleranceFromY = 70; //From ±90 degrees in each direction from start point
+    private final double beltGyroRotationInsideDegreeToleranceFromY = 70; //From ±90 degrees in each direction from start point
     
     public void runComponents(Components components, ControlInputs controlInputs, SensorInputs sensorInputs) {
         
@@ -14,9 +14,9 @@ public class ComponentsControl {
         //Controls
             //Belts
         if (controlInputs.beltAuto) {
-            if (sensorInputs.currentYawDegrees <= -(90-beltGyroRotaionInsideDegreeToleranceFromY) && sensorInputs.currentYawDegrees >= -(90+beltGyroRotaionInsideDegreeToleranceFromY)) {
+            if (sensorInputs.currentYawDegrees <= -(90 - beltGyroRotationInsideDegreeToleranceFromY) && sensorInputs.currentYawDegrees >= -(90 + beltGyroRotationInsideDegreeToleranceFromY)) {
                 mainSideBeltSpeed = beltK;
-            } else if (sensorInputs.currentYawDegrees >= (90-beltGyroRotaionInsideDegreeToleranceFromY) && sensorInputs.currentYawDegrees <= (90+beltGyroRotaionInsideDegreeToleranceFromY)) {
+            } else if (sensorInputs.currentYawDegrees >= (90 - beltGyroRotationInsideDegreeToleranceFromY) && sensorInputs.currentYawDegrees <= (90 + beltGyroRotationInsideDegreeToleranceFromY)) {
                 mainSideBeltSpeed = -beltK;
             }
         } else {
